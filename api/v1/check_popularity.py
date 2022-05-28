a5db4d3186e1b26064edf0f215ea2979211fe950
Omid Zarinmahd <omid.z@rahgoshagroup.com>
2022-05-28 17:26:54 +0300

    validation for url

--- api/v1/check_popularity.py
+++ api/v1/check_popularity.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
+from pydantic import BaseModel, validator
+
 from services.github_api import GithubApi
-
-from pydantic import BaseModel, HttpUrl
 from utility.custom_renderer import CustomResponse
-
 
 __all__ = ["check_popularity_router"]
 
@@ -11,7 +10,24 @@ check_popularity_router = APIRouter(prefix="/check_popularity")
 
 
 class RepoAddress(BaseModel):
-    repo_address: HttpUrl
+    repo_address: str
+
+    @validator("repo_address")
+    def validate_repo_address(cls, v: str):
+        if "http" not in v or "https" not in v or "/" not in v:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="the address you interned is not valid",
+            )
+
+        v = v.split("/")
+        if len(v) < 2:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="the address you entered is not correct, it should contain owner/repo_name"
+            )
+
+        return "/".join(v[-2:])
 
 
 @check_popularity_router.post(path="/", status_code=status.HTTP_200_OK)
