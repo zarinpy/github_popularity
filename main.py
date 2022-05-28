662943c5b334bdb93ed5856961592d1d4cbd05ca
Omid Zarinmahd <omid.z@rahgoshagroup.com>
2022-05-28 17:32:22 +0300

    add custom exception handler

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 
 from api import api
 from setting import setting
+from utility.custom_renderer import CustomResponse
 
 app = FastAPI(
     title="fast-API Github App",
     version="0.0.1",
     docs_url=setting.DOC_URL,
 )
+
+
+@app.exception_handler(HTTPException)
+def http_exception_handler(request: Request, exc: HTTPException):
+    return CustomResponse(
+        status_code=exc.status_code,
+        message=exc.detail,
+        content={},
+    )
+
 
 app.include_router(router=api, prefix="/api/v1")
 
